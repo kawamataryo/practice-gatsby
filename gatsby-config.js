a73de74a9d91e8a3fbea98b8e81156f920bc409d
kawamataryou <ba068082@gmail.com>
2020-03-08 07:16:09 +0900

    [from now] 2020/03/08 07:16:09
    diff --git a/gatsby-config.js b/gatsby-config.js
    index 1996136..859cdf0 100644
    --- a/gatsby-config.js
    +++ b/gatsby-config.js
    @@ -11,7 +11,7 @@ module.exports = {
         author: {
           name: "Resi Respati",
           url: "https://twitter.com/resir014",
    -      email: "resir014@gmail.com"
    +      email: "resir014@gmail.coms"
         }
       },
       plugins: [

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     author: {
       name: "Resi Respati",
       url: "https://twitter.com/resir014",
-      email: "resir014@gmail.com"
+      email: "resir014@gmail.coms"
     }
   },
   plugins: [
